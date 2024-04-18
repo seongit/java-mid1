66ce878e46729aa7a5dacc7f0d60eed163bc6c5c
SeongEun Kim <80396754+seongit@users.noreply.github.com>
2024-04-19 07:55:39 +0900

    feat(Immutable Class): 실전 자바 중급 1편
    불변 객체 값 변경

---
+++ src/lang/immutable/change/ImmutableMain.java
@@ -0,0 +1,10 @@
+package lang.immutable.change;
+
+public class ImmutableMain {
+    public static void main(String[] args) {
+        ImmutableObj obj1 = new ImmutableObj(10);
+        ImmutableObj obj2 = obj1.add(20);
+
+        System.out.println("ImmutableMain.main " + "obj1 " + obj1.getValue() + " ojb2 " + obj2.getValue());
+    }
+}
